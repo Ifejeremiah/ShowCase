3257aba9ed081165ad46ba83f9ea8e507df38051
Ife Jeremiah <73410455+Ifejeremiah@users.noreply.github.com>
2021-02-07 22:28:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper')
+const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper');
+const inner = document.querySelector('.inner');
+const rightSide = document.querySelector('.right-side');
 
 portfolioItems.forEach(portfolioItem => {
     portfolioItem.addEventListener('mouseover', () => {
@@ -10,5 +12,9 @@ portfolioItems.forEach(portfolioItem => {
         portfolioItem.childNodes[1].classList.remove('img-darken');
 
     })
-}
-    )
+})
+
+rightSide.addEventListener('click', () => {
+    inner.classList.toggle('menu');
+    rightSide.classList.toggle('right-inner');
+})
